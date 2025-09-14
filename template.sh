dba039a3af2ecb971480a2bb02f445a137b397a1
blacksayan6 <sayanroychoudhury000@gmail.com>
2025-09-14 22:54:10 +0530

    initial commit

---
+++ template.sh
@@ -0,0 +1,11 @@
+mkdir -p sayan
+mkdir -p research
+touch app.py
+touch sayan/__init__.py
+touch sayan/helper.py
+touch sayan/promt.py
+touch .evn
+touch setup.py
+touch requerments.txt
+touch research/troals.ipynb
+echo "directory and filees created successfully"
